3d512fd16fcf4c8907e206bd438d8006a6f093d9
Baichuan Chi <baichuan.chi@gmail.com>
2020-06-03 21:13:32 -0500

    name change for 1-12 and upload 1-20

---
+++ zyLab1-20.py
@@ -0,0 +1,10 @@
+#Name:Baichuan Chi
+#PSID:1938207
+
+user_num1 = int(input('Enter integer:\n'))
+print('You entered:', user_num1)
+print(user_num1, "squared is", user_num1*user_num1)
+print('And', user_num1, 'cubed is',user_num1*user_num1*user_num1, '!!')
+user_num2 = int(input('Enter another integer:\n'))
+print(user_num1, '+',user_num2, 'is', user_num1+user_num2)
+print(user_num1, '*',user_num2, 'is', user_num1*user_num2)
