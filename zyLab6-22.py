81a1a9e9c5c4a59f390a77df695bec0815616fe9
Baichuan Chi <baichuan.chi@gmail.com>
2020-06-10 12:47:41 -0500

    zyLab6-22.py not yet finished, backup

---
+++ zyLab6-22.py
@@ -0,0 +1,25 @@
+# Name:Baichuan Chi
+# PSID:1938207
+
+# a1*x+b1*y=c1
+# a2*x+b2*y=c2
+a1 = int(input())
+b1 = int(input())
+c1 = int(input())
+a2 = int(input())
+b2 = int(input())
+c2 = int(input())
+
+x = 0
+y = 0
+
+for x in range(-10, 11):
+    for y in range(-10, 11):
+        result1 = a1*x+b1*y
+        result2 = a2*x+b2*y
+        if result1 == c1 and result2 == c2:
+            print(x, y)
+            break
+        break
+else:
+    print('No solution')
