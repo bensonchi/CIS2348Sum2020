49315274579a322cf70af88c959df45a020bca28
Baichuan Chi <baichuan.chi@gmail.com>
2020-06-15 09:14:58 -0500

    coding_problem2 Part C with small improvement. Added a else loop in the end to handle unwanted entries and modified the first elif loop as numbers are included in the last else condition.

--- coding_problem_2.py
+++ coding_problem_2.py
@@ -5,12 +5,12 @@
 month_num = {'January': '1', 'February': '2', 'March': '3', 'April': '4', 'May': '5', 'June': '6', 'July': '7',
              'August': '8', 'September': '9', 'October': '10', 'November': '11', 'December': '12'}
 
-with open('inputDates.txt', 'r') as f:
-    with open('parsedDates.txt', 'w') as w:
+with open('inputDates.txt', 'r') as f:  # open file for read
+    with open('parsedDates.txt', 'w') as w:  # open file or create file for write
         for line in f:
             if line == '-1':
                 break
-            elif '0' <= line[0] <= '9' or '.' in line:  # ignore entries that starts with numbers or has abbrev(Nov.)
+            elif '.' in line:  # ignore entries that includes  abbrev like 'Nov.')
                 continue
             elif 'A' <= line[0] <= 'Z':
                 user_split = line.split(', ')
@@ -18,6 +18,5 @@ with open('inputDates.txt', 'r') as f:
                 month_split = month_day.split(' ')
                 month, day = month_num[month_split[0]], month_split[1]
                 w.write('{}/{}/{}'.format(month, day, year))  # modified to write to file instead of print screen
-
-
-
+            else:
+                continue
