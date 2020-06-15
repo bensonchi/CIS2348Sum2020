1fe43f378a5194b8c84b3924ff9a76e01990021c
Baichuan Chi <baichuan.chi@gmail.com>
2020-06-15 09:05:50 -0500

    coding_problem2 part-c finished

--- coding_problem_2.py
+++ coding_problem_2.py
@@ -6,16 +6,18 @@ month_num = {'January': '1', 'February': '2', 'March': '3', 'April': '4', 'May':
              'August': '8', 'September': '9', 'October': '10', 'November': '11', 'December': '12'}
 
 with open('inputDates.txt', 'r') as f:
-    for line in f:
-        if line == '-1':
-            break
-        elif '0' <= line[0] <= '9' or '.' in line:  # ignore entries that starts with numbers or has abbrev like 'Nov.'
-            continue
-        elif 'A' <= line[0] <= 'Z':
-            user_split = line.split(', ')
-            month_day, year = user_split[0], user_split[1]
-            month_split = month_day.split(' ')
-            month, day = month_num[month_split[0]], month_split[1]
-            print('{}/{}/{}'.format(month, day, year), end='')
+    with open('parsedDates.txt', 'w') as w:
+        for line in f:
+            if line == '-1':
+                break
+            elif '0' <= line[0] <= '9' or '.' in line:  # ignore entries that starts with numbers or has abbrev(Nov.)
+                continue
+            elif 'A' <= line[0] <= 'Z':
+                user_split = line.split(', ')
+                month_day, year = user_split[0], user_split[1]
+                month_split = month_day.split(' ')
+                month, day = month_num[month_split[0]], month_split[1]
+                w.write('{}/{}/{}'.format(month, day, year))  # modified to write to file instead of print screen
+
 
 
