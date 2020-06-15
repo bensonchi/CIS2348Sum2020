0dc0daa2c38e3eef30b043e9c0fdc47050fa6c54
Baichuan Chi <baichuan.chi@gmail.com>
2020-06-15 08:57:34 -0500

    coding_problem2 part-b finished

--- coding_problem_2.py
+++ coding_problem_2.py
@@ -5,12 +5,17 @@
 month_num = {'January': '1', 'February': '2', 'March': '3', 'April': '4', 'May': '5', 'June': '6', 'July': '7',
              'August': '8', 'September': '9', 'October': '10', 'November': '11', 'December': '12'}
 
-user_date = input('Please enter a date in the format of \'March 1, 1990\': ')
-if user_date != '-1':
-    user_split = user_date.split(', ')
-    month_day, year = user_split[0], user_split[1]
-    month_split = month_day.split(' ')
-    month, day = month_num[month_split[0]], month_split[1]
-    print('{}/{}/{}'.format(month, day, year))
-else:
-    print('Thank you')
+with open('inputDates.txt', 'r') as f:
+    for line in f:
+        if line == '-1':
+            break
+        elif '0' <= line[0] <= '9' or '.' in line:  # ignore entries that starts with numbers or has abbrev like 'Nov.'
+            continue
+        elif 'A' <= line[0] <= 'Z':
+            user_split = line.split(', ')
+            month_day, year = user_split[0], user_split[1]
+            month_split = month_day.split(' ')
+            month, day = month_num[month_split[0]], month_split[1]
+            print('{}/{}/{}'.format(month, day, year), end='')
+
+
