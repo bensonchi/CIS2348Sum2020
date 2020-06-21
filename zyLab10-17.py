abbaabc8165719325003d58a93f0f25430ee5d38
Baichuan Chi <baichuan.chi@gmail.com>
2020-06-21 17:23:44 -0500

    zyLab10-17.py final edition, added name == main, passed zyLab test

--- zyLab10-17.py
+++ zyLab10-17.py
@@ -13,26 +13,27 @@ class ItemToPurchase:
         print('{} {} @ ${} = ${}'.format(self.item_name, self.item_quantity, self.item_price, total_price))
 
 
-print('Item 1')  # Prompt user to input information for item 1.
-print('Enter the item name:')
-name1 = input()
-print('Enter the item price:')
-price1 = int(input())
-print('Enter the item quantity:')
-quantity1 = int(input())
+if __name__ == '__main__':
+    print('Item 1')  # Prompt user to input information for item 1.
+    print('Enter the item name:')
+    name1 = input()
+    print('Enter the item price:')
+    price1 = int(input())
+    print('Enter the item quantity:')
+    quantity1 = int(input())
 
-print('\nItem 2')  # Prompt user to input information for item 1.
-print('Enter the item name:')
-name2 = input()
-print('Enter the item price:')
-price2 = int(input())
-print('Enter the item quantity:')
-quantity2 = int(input())
+    print('\nItem 2')  # Prompt user to input information for item 1.
+    print('Enter the item name:')
+    name2 = input()
+    print('Enter the item price:')
+    price2 = int(input())
+    print('Enter the item quantity:')
+    quantity2 = int(input())
 
-item1 = ItemToPurchase(name1, price1, quantity1)  # Create two objects of class ItemToPurchase
-item2 = ItemToPurchase(name2, price2, quantity2)
-print()  # blank line for format
-print('TOTAL COST')
-item1.print_item_cost()
-item2.print_item_cost()
-print('\nTotal: ${}'.format(quantity1*price1+quantity2*price2))
+    item1 = ItemToPurchase(name1, price1, quantity1)  # Create two objects of class ItemToPurchase
+    item2 = ItemToPurchase(name2, price2, quantity2)
+    print()  # blank line for format
+    print('TOTAL COST')
+    item1.print_item_cost()
+    item2.print_item_cost()
+    print('\nTotal: ${}'.format(quantity1 * price1 + quantity2 * price2))
